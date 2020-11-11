fdb8717a18cf3f17d1244e2d3addc1e054f4af29
ricardo.ferreira.costa08@gmail.com <ricardo.ferreira.costa08@gmail.com>
2020-11-11 17:58:00 -0300

    project's name changed

--- web/env.php
+++ web/env.php
@@ -1,4 +1,4 @@
 <?php
-$_ENV['URL_API_BOOKS'] = "http://microservice_api-books_1:5000/";
-$_ENV['URL_API_READERS'] = "http://microservice_api-readers_1:5000/";
+$_ENV['URL_API_BOOKS'] = "http://microservice-simple-docker-compose_api-books_1:5000/";
+$_ENV['URL_API_READERS'] = "http://microservice-simple-docker-compose_api-readers_1:5000/";
 ?>
